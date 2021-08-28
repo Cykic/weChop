712475bc527ad0b89a6ef481ca498f15552fc722
cykic <cyb3rcykic@gmail.com>
2021-08-28 08:27:25 +0100

    Postman

--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -6,9 +6,7 @@ const router = express.Router();
 router.all(
   '/',
   catchAsync(async (req, res, next) => {
-    res.redirect(
-      'https://documenter.getpostman.com/view/15594941/TzzAMcSF#434e804b-831b-41e5-b48e-74ca6109b04b'
-    );
+    res.redirect(process.env.POSTMAN_DOC);
   })
 );
 
