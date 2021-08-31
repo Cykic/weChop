d4a7322638b36cee367802beaa36ca6be21dc56f
cykic <cyb3rcykic@gmail.com>
2021-09-01 00:01:00 +0100

    Feat: Added expire time to OTP

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -9,40 +9,53 @@ const authController = require('./authController');
 exports.signup = catchAsync(async (req, res, next) => {
   const otp = OTP.generate();
 
+  const expireTime = Date.now() + process.env.OTP_EXPIRES_MINUTES * 60000;
   const admin = new Admin();
   const { name, phone, password, confirmPassword } = req.body;
+  const message = `Your otp for Admin of weChop is ${otp.code} and it expires in ${process.env.OTP_EXPIRES_MINUTES} minutes`;
 
   admin.name = name;
   admin.phone = phone;
   admin.password = password;
   admin.confirmPassword = confirmPassword;
   admin.verificationCode = otp.hash;
+  admin.verificationExpires = expireTime;
 
   await admin.save();
-  await sendSms(phone, `Your otp for Admin of weChop is ${otp.code}`);
+  await sendSms(phone, message);
 
   res.status(201).json({
     status: 'success',
-    message: 'Your admin account has been created'
+    message: 'Your admin account has been created',
+    data: {
+      admin
+    }
   });
 });
 
 exports.verify = catchAsync(async (req, res, next) => {
-  if (!req.body.code) return next(new AppError('Please provide code', 400));
+  if (!req.body.code || !req.body.id)
+    return next(new AppError('Please provide code and id', 400));
 
-  const { code } = req.body;
+  const { code, id } = req.body;
 
   const verificationCode = OTP.hash(code);
 
-  const admin = await Admin.findOneAndUpdate(
-    { verificationCode },
-    { verified: true, verifiedAt: Date.now() },
-    { new: true, select: 'name phone role verified' }
-  );
+  const admin = await Admin.findById(id).select('+verificationCode');
 
-  if (!admin) {
-    return next(new AppError('Verification failed, Invalid code', 400));
-  }
+  if (!admin) return next(new AppError('No Account found', 404));
+
+  if (Date.now() > admin.verificationExpires)
+    return next(new AppError('Verification code has expired', 400));
+
+  if (verificationCode !== admin.verificationCode)
+    return next(new AppError('Verification code is not valid', 400));
+
+  admin.verified = true;
+  admin.verifiedAt = Date.now();
+
+  await admin.save({ validateBeforeSave: false });
+
   res.status(200).json({
     status: 'success',
     data: { admin }
